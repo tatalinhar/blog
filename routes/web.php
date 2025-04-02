569604975ee8363f487385368e0f832eb62f90b1
tatalinhar <taualeite@gmail.com>
2025-04-01 21:32:29 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controlles\ProdutoController;
 use App\Http\Controllers\ProdutoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/produto', [ProdutoController::class,'index']);
