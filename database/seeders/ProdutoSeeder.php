31fc9fec356ef6ef7c52dcb18faf3e1fe726ad74
carolinemusa <carolinedossantos2501@gmail.com>
2025-04-01 21:50:07 -0300

    migrate 20m

---
+++ database/seeders/ProdutoSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProdutoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('produtos')->insert([
+            'nome' => 'Sapato',
+            'quantidade' => 10,
+            'valor' => 100.10,
+        ]);
+    }
+}
