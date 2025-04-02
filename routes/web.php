2c94604772cc66194c7f06daa27b007b0e901de2
tatalinhar <taualeite@gmail.com>
2025-04-01 21:30:54 -0300

    aula1

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controlles\ProdutoController;
+use App\Http\Controllers\ProdutoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/produto',[ProdutoController::class,'index']);
+Route::get('/produto',[ProdutoController::class,'index']);Route::get('/produto', [ProdutoController::class,'index']);
