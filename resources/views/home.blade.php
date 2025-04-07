8df7933d7573997a79c06f5893a04bc914ac70b0
tatalinhar <taualeite@gmail.com>
2025-04-07 20:20:07 -0300

    video

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('Hy bitch!') }}
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/produto', [ProdutoController::class,'index']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
