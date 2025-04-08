1ea9bcb2ed93a643e889175db876d526a0ba27e5
tatalinhar <taualeite@gmail.com>
2025-04-07 21:26:37 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ProdutoSeeder::class,
-
+            CategoriaSeeder::class,
         ]);
     }
 }
