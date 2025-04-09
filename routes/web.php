022b8f26b6c0c56291d617e87ed2362a793d4dcf
tatalinhar <taualeite@gmail.com>
2025-04-08 21:06:34 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\CategoriaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/produto', [ProdutoController::class,'index']);
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/produto', [ProdutoController::class,'index'])->name('home');
+Route::get('/categorias', [CategoriaController::class,'index'])->name('categoria.index');
