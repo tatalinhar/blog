e35eaa28a84e92c12d4b48556a041e28330b8793
tatalinhar <taualeite@gmail.com>
2025-04-10 22:01:05 -0300

    crud3

--- resources/views/categoria/categoria_show.blade.php
+++ resources/views/categoria/categoria_show.blade.php
@@ -10,11 +10,10 @@
                 <div class="card-body">
 
 
-                    <p><strong>Id: </strong>{{ $categoria->id  }}</p>
-                    <p><strong>Nome: </strong>{{ $categoria->nome  }}</p>
-                    <p><strong>Criaçâo: </strong>{{ $categoria->created_at  }}</p>
-                    <p><strong>Atualização: </strong>{{ $categoria->updated_at  }}</p>
-
+                    <p><strong>Id: </strong>{{ $categoria->id }}</p>
+                    <p><strong>Nome: </strong>{{ $categoria->nome }}</p>
+                    <p><strong>Criação: </strong>{{ $categoria->created_at }}</p>
+                    <p><strong>Atualização: </strong>{{ $categoria->updated_at }}</p>
 
 
 
