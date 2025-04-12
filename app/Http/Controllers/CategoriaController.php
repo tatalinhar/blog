2eec08ecfdc44461b7fd668a10567991c51f3b28
tatalinhar <taualeite@gmail.com>
2025-04-11 21:28:47 -0300

    Update CategoriaController.php

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -26,7 +26,7 @@ class CategoriaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-     public function store(Request $request)
+    public function store(Request $request)
     {
 
         $messages = [
