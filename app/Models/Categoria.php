89b610ee8906ae8017f391c6456b042e9c69a01c
carolinemusa <carolinedossantos2501@gmail.com>
2025-05-16 20:21:18 -0300

    BLOG - Protegendo rotas com Auth(00:00)

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Categoria extends Model
+class Categoria extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
     protected $table = 'categorias';
 }
